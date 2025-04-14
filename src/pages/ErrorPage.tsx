cd149b3e21772b000a15144d6da26cf2a4983c60
Robert <robertnostor@gmail.com>
2025-04-14 19:16:07 +0300

    Updated ErrorPage.tsx file

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,17 @@
 export default function ErrorPage(error: string) {
   return (
-    <>
-      <div>An error occured: {error}</div>
-    </>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-red-400 via-purple-200 to-blue-400 text-white p-4">
+      <h1 className="text-6xl font-extrabold mb-4 drop-shadow-lg">404</h1>
+      <p className="text-2xl font-semibold mb-2">Oops </p>
+      <p className="text-md mb-6 text-center max-w-md">
+        {error}
+      </p>
+      <a
+        href="/"
+        className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-6 rounded shadow-md"
+      >
+        Go to Dashboard
+      </a>
+    </div>
   );
 }
