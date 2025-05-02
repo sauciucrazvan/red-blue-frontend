842722586c920e1a15de8b8c04b6e91558e92503
Robert <robertnostor@gmail.com>
2025-05-02 17:48:21 +0300

    Updated WaitingLobby design & copyToClipboard() added

--- src/pages/WaitingLobby.tsx
+++ src/pages/WaitingLobby.tsx
@@ -55,12 +55,28 @@ export default function WaitingLobby(props: WaitingLobbyProps) {
     initializeWebSocket();
   }, [props.id]);
 
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(props.game_code);
+      console.log("Game code copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy:", err);
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-700 via-purple-300 to-blue-700 text-gray-800">
       <div className="bg-black bg-opacity-10 backdrop-blur-md border border-white border-opacity-30 rounded-2xl shadow-xl p-10 max-w-md w-full text-center space-y-6">
         <h2 className="text-2xl font-bold">Waiting for the opponent...</h2>
         <p className="text-lg">Share this code with your opponent: </p>
-        <div>{props.game_code}</div>
+        <div
+          onClick={copyToClipboard}
+          className="cursor-pointer underline underline-offset-2 text-lg text-gray-800 hover:text-gray-300 transition"
+          title="Click to copy"
+        >
+          {props.game_code}
+        </div>
+
         <p className="text-sm text-gray-700 italic">
           The game will start once your opponent joins.
         </p>
