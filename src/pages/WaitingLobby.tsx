e2386a52a6244c039a8c7e08b720613cf344f7dd
Robert <robertnostor@gmail.com>
2025-05-02 17:47:03 +0300

    Updated WaitingLobby design & copyToClipboard() added

--- src/pages/WaitingLobby.tsx
+++ src/pages/WaitingLobby.tsx
@@ -56,7 +56,7 @@ export default function WaitingLobby(props: WaitingLobbyProps) {
   }, [props.id]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-400 via-purple-200 to-blue-400 text-gray-800">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-700 via-purple-300 to-blue-700 text-gray-800">
       <div className="bg-black bg-opacity-10 backdrop-blur-md border border-white border-opacity-30 rounded-2xl shadow-xl p-10 max-w-md w-full text-center space-y-6">
         <h2 className="text-2xl font-bold">Waiting for the opponent...</h2>
         <p className="text-lg">Share this code with your opponent: </p>
